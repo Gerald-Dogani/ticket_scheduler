8bb9816449d135de327ec5c816a226f7e4c50f88
Gerald Dogani <doganigerald@gmail.com>
2022-07-17 20:44:41 +0200

    adding version on footer

--- src/app/core/components/navigation/footer/footer.component.ts
+++ src/app/core/components/navigation/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as packageInfo from "@core/../../package.json";
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  public version = packageInfo;
   constructor() { }
 
   ngOnInit(): void {
